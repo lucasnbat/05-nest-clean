48b41b7439b55b9a1fc0bfea219201edb1dce39e
Lucas Batista <lucasnunesb6@gmail.com>
2024-11-04 11:18:30 -0400

    feat: connection with db and project using prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 // adicionar algo aqui faz virar prefixo para todas as rotas
 @Controller('/api')
 export class AppController {
   // conceito de inversão de dependência aqui, recebe AppService como prop.
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get('/hello')
   index(): string {
     return this.appService.getHello()
+  }
+
+  @Post('/hello')
+  async store() {
+    return await this.prisma.user.findMany()
   }
 }
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+// @Injectable()
+// export class PrismaService {
+//   // atributo e tipagem
+//   public client: PrismaClient
+
+//   // instanciação com construtor
+//   constructor() {
+//     this.client = new PrismaClient()
+//   }
+// }
+
+// essa é a versão final. veja que extendo as props de PrismaClient e apenas
+// chamo os métodos dela com o super() (ele chama o constructor da PrismaClient)
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  public client: PrismaClient
+
+  constructor() {
+    super({
+      log: ['error', 'warn'],
+    })
+  }
+
+  // nest chama isso ao instanciar módulo que usa esse serviço for criado
+  onModuleInit() {
+    return this.$connect // conecte com prisma
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect // desconecta com prisma ao módulo ser destruído
+  }
+}
