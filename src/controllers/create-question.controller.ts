f92c567614944e3123dad3a3b1427f3b92c04ef1
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-11-12 16:42:24 -0400

    feat: add post questions route

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayloadType } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
-// minute: 01:56
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodyType = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
 @Controller('/questions')
 // uso para acionar meu jwt.strategy.ts e proteger a rota
 // usamos 'jwt' porque estmaos usando jwt strategy do passport
@@ -11,13 +22,39 @@ import { UserPayloadType } from 'src/auth/jwt.strategy'
 // atualizei para usar uma classe JwtAuthGuard que instancia um AuthGuard('jwt')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prismaDependency: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayloadType) {
+  async handle(
+    @CurrentUser() user: UserPayloadType,
+    @Body(bodyValidationPipe) body: CreateQuestionBodyType,
+  ) {
+    const { content, title } = body
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prismaDependency.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
     // aparentemente é possível buscar o user porque é uma rota autenticada
     // então o sistema de módulos extrai o user a partir do token
-    console.log(user.sub)
+    // console.log(user.sub)
     return 'ok'
+  }
+
+  // função para conversão para slug
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
   }
 }
