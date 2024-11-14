3e2a84d1c539d9a4023ab0367e259d7ef54e280b
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-11-14 16:28:04 -0400

    feat: finish `fetch-recent-questions.controllre.ts`]

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number) // isso nao valida, ele transforma para number
+  .pipe(z.number().min(1)) // o pipe valida considerando todas as outras validações anteriores como um resultado só
+
+type PageQueryParamType = z.infer<typeof pageQueryParamsSchema>
+
+// preparando o pipe
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema)
 
 @Controller('/questions')
 // uso para acionar meu jwt.strategy.ts e proteger a rota
@@ -12,5 +26,22 @@ export class FetchRecentQuestionsController {
   constructor(private prismaDependency: PrismaService) {}
 
   @Get()
-  async handle() {}
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamType) {
+    /* 
+     * Se tá na página 1, não pula nada e mostra apenas a pergunta mais recente 
+    (lembre: é uma por página). 
+     * Se tá na página 2, pula uma (a primeira mais recente) e mostra a segunda,
+    que é a segunda pergunta mais recente 
+     */
+    const perPage = 1
+    const questions = await this.prismaDependency.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createAt: 'desc', // ordena das mais recentes para mais antigas
+      },
+    })
+
+    return { questions }
+  }
 }
--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,9 +1,9 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common'
-import { ZodError, ZodObject } from 'zod'
+import { ZodError, ZodSchema } from 'zod'
 import { fromZodError } from 'zod-validation-error'
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodObject<any>) {}
+  constructor(private schema: ZodSchema) {}
 
   /*
    * Método padrão dos pipes é o transform(). Ele pega a operação e executa
@@ -13,7 +13,7 @@ export class ZodValidationPipe implements PipeTransform {
    */
   transform(value: unknown) {
     try {
-      this.schema.parse(value)
+      return this.schema.parse(value)
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({
@@ -25,6 +25,5 @@ export class ZodValidationPipe implements PipeTransform {
 
       throw new BadRequestException('Validation failed')
     }
-    return value
   }
 }
