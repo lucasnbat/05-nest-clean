8f6d97b713557fa160a086fff77fcfa2ca64dee2
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-27 13:35:45 -0400

    feat: create upload attahcment controller with test

--- src/infra/http/controllers/upload-attachment.controller.ts
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 
 @Controller('/attachments')
-export class GetQuestionBySlugController {
+export class UploadAttachmentController {
   //   constructor() {}
 
   @Post()
   // usado para upload de arquivos usando multer
   // 'file' é o nome dado ao anexo
   @UseInterceptors(FileInterceptor('file'))
-  async handle(@UploadedFile() file: Express.Multer.File) {}
+  async handle(
+    @UploadedFile(
+      // validações
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }), // 2 MB
+          new FileTypeValidator({ fileType: '.(png|jpg|jpeg|pdf)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file)
+  }
 }
