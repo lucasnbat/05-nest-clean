ab2a34739413b02a2bdfdfaf32084f5e4b8dd780
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-27 15:21:57 -0400

    feat: add upload and create attachment use case test with fake uploader contract, fake uploader in memory implementation and attachments repository in memory implementation

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
@@ -0,0 +1,50 @@
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
+import { UploadAndCreateAttachmentUseCase } from './upload-and-create-attachment'
+import { FakeUploader } from 'test/storage/fake-uploader'
+import { InvalidAttachmentType } from './errors/invalid-attachment-type'
+
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let fakeUploader: FakeUploader
+let sut: UploadAndCreateAttachmentUseCase
+
+describe('Upload and create attachment', () => {
+  beforeEach(() => {
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    fakeUploader = new FakeUploader()
+    sut = new UploadAndCreateAttachmentUseCase(
+      inMemoryAttachmentsRepository,
+      fakeUploader,
+    )
+  })
+
+  it('should be able to upload and create an attachment', async () => {
+    const result = await sut.execute({
+      fileName: 'test-upload.pdf',
+      fileType: 'application/pdf',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      attachment: inMemoryAttachmentsRepository.items[0],
+    })
+
+    expect(fakeUploader.uploads).toHaveLength(1)
+    expect(fakeUploader.uploads[0]).toEqual(
+      expect.objectContaining({
+        fileName: 'test-upload.pdf',
+      }),
+    )
+  })
+
+  it('should not be able to upload and attachment with invalid file type', async () => {
+    const result = await sut.execute({
+      fileName: 'test-upload.mp3',
+      fileType: 'audio/mpeg',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidAttachmentType)
+  })
+})
