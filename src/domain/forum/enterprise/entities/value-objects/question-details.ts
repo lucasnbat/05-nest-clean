afb52920ac56d40adc9aac4fbfe970e66a5a4e7a
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-30 13:30:54 -0400

    docs: working on question details value object

---
+++ src/domain/forum/enterprise/entities/value-objects/question-details.ts
@@ -0,0 +1,47 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ValueObject } from '@/core/entities/value-object'
+import { Slug } from './slug'
+import { Attachment } from '../attachment'
+
+export interface QuestionDetailsProps {
+  questionId: UniqueEntityID
+  authorId: UniqueEntityID
+  autor: string
+  title: string
+  slug: Slug
+  content: string
+  attachments: Attachment[]
+  bestAnswerId?: UniqueEntityID | null
+  createdAt: Date
+  updatedAt: Date | null
+}
+
+export class QuestionDetails extends ValueObject<QuestionDetailsProps> {
+  get commentId() {
+    return this.props.commentId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get author() {
+    return this.props.author
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(props: CommentWithAuthorProps) {
+    return new CommentWithAuthor(props)
+  }
+}
