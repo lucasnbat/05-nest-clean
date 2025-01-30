f175635de2b1750fe59754d783953d58443c38e8
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-30 10:10:29 -0400

    feat: fetch question comments use case returning comments with authors with success

--- src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -3,6 +3,7 @@ import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-
 import { FetchQuestionCommentsUseCase } from './fetch-question-comments'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
 import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { makeStudent } from 'test/factories/make-student'
 
 let inMemoryQuestionCommentsRepositoryInstance: InMemoryQuestionCommentsRepository
 let inMemoryStudentsRepositoryInstance: InMemoryStudentsRepository
@@ -20,35 +21,67 @@ describe('Fetch Question Comments', () => {
   })
 
   it('should be able to fetch question comments', async () => {
-    await inMemoryQuestionCommentsRepositoryInstance.create(
-      makeQuestionComment({
-        questionId: new UniqueEntityID('question-1'),
-      }),
-    )
-    await inMemoryQuestionCommentsRepositoryInstance.create(
-      makeQuestionComment({
-        questionId: new UniqueEntityID('question-1'),
-      }),
-    )
-    await inMemoryQuestionCommentsRepositoryInstance.create(
-      makeQuestionComment({
-        questionId: new UniqueEntityID('question-1'),
-      }),
-    )
+    const student = makeStudent({ name: 'John Doe' })
+
+    // equivale a usar o create()
+    inMemoryStudentsRepositoryInstance.items.push(student)
+
+    const comment1 = makeQuestionComment({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    })
+    const comment2 = makeQuestionComment({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    })
+    const comment3 = makeQuestionComment({
+      questionId: new UniqueEntityID('question-1'),
+      authorId: student.id,
+    })
+
+    await inMemoryQuestionCommentsRepositoryInstance.create(comment1)
+    await inMemoryQuestionCommentsRepositoryInstance.create(comment2)
+    await inMemoryQuestionCommentsRepositoryInstance.create(comment3)
 
     const result = await sut.execute({
       questionId: 'question-1',
       page: 1,
     })
 
-    expect(result.value?.questionComments).toHaveLength(3)
+    expect(result.value?.comments).toHaveLength(3)
+    expect(result.value?.comments).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment1.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment2.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment3.id,
+        }),
+      ]),
+    )
   })
 
   it('should be able to fetch question comments', async () => {
+    // agora sempre que criar questionComment tem que passar o author
+    // que no caso será esse student aqui...isso porque se você lembrar
+    // la no in memory question comments repo a gente sinalizou que se
+    // não tiver author (!author) é pra dar um erro (throw new Error())
+    const student = makeStudent({ name: 'John Doe' })
+
+    // equivale a usar o create()
+    inMemoryStudentsRepositoryInstance.items.push(student)
+
     for (let i = 1; i <= 22; i++) {
       await inMemoryQuestionCommentsRepositoryInstance.create(
         makeQuestionComment({
           questionId: new UniqueEntityID('question-1'),
+          authorId: student.id,
         }),
       )
     }
@@ -60,6 +93,6 @@ describe('Fetch Question Comments', () => {
     })
 
     // espera-se que só liste 2 registros
-    expect(result.value?.questionComments).toHaveLength(2)
+    expect(result.value?.comments).toHaveLength(2)
   })
 })
