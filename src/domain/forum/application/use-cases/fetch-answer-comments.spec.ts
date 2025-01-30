f2ec314ad7660754d4f4aeffdf1fc729440625df
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-01-30 10:55:18 -0400

    docs: working on fetch anssswer comments test. Minute 02:50

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -1,62 +1,62 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { FetchAnswerCommentsUseCase } from "./fetch-answer-comments";
-import { makeAnswerComment } from "test/factories/make-answer-comment";
-import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments'
+import { makeAnswerComment } from 'test/factories/make-answer-comment'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
 
-let inMemoryAnswerCommentsRepositoryInstance: InMemoryAnswerCommentsRepository;
-let sut: FetchAnswerCommentsUseCase;
+let inMemoryAnswerCommentsRepositoryInstance: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
 
-describe("Fetch Answer Comments", () => {
+describe('Fetch Answer Comments', () => {
   beforeEach(() => {
     inMemoryAnswerCommentsRepositoryInstance =
-      new InMemoryAnswerCommentsRepository();
+      new InMemoryAnswerCommentsRepository()
 
     sut = new FetchAnswerCommentsUseCase(
       inMemoryAnswerCommentsRepositoryInstance,
-    );
-  });
+    )
+  })
 
-  it("should be able to fetch answer comments", async () => {
+  it('should be able to fetch answer comments', async () => {
     await inMemoryAnswerCommentsRepositoryInstance.create(
       makeAnswerComment({
-        answerId: new UniqueEntityID("answer-1"),
+        answerId: new UniqueEntityID('answer-1'),
       }),
-    );
+    )
     await inMemoryAnswerCommentsRepositoryInstance.create(
       makeAnswerComment({
-        answerId: new UniqueEntityID("answer-1"),
+        answerId: new UniqueEntityID('answer-1'),
       }),
-    );
+    )
     await inMemoryAnswerCommentsRepositoryInstance.create(
       makeAnswerComment({
-        answerId: new UniqueEntityID("answer-1"),
+        answerId: new UniqueEntityID('answer-1'),
       }),
-    );
+    )
 
     const result = await sut.execute({
-      answerId: "answer-1",
+      answerId: 'answer-1',
       page: 1,
-    });
+    })
 
-    expect(result.value?.answerComments).toHaveLength(3);
-  });
+    expect(result.value?.answerComments).toHaveLength(3)
+  })
 
-  it("should be able to fetch answer comments", async () => {
+  it('should be able to fetch answer comments', async () => {
     for (let i = 1; i <= 22; i++) {
       await inMemoryAnswerCommentsRepositoryInstance.create(
         makeAnswerComment({
-          answerId: new UniqueEntityID("answer-1"),
+          answerId: new UniqueEntityID('answer-1'),
         }),
-      );
+      )
     }
 
     // pedindo por página 2...
     const result = await sut.execute({
-      answerId: "answer-1",
+      answerId: 'answer-1',
       page: 2,
-    });
+    })
 
     // espera-se que só liste 2 registros
-    expect(result.value?.answerComments).toHaveLength(2);
-  });
-});
+    expect(result.value?.answerComments).toHaveLength(2)
+  })
+})
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,46 +1,85 @@
-import { PaginationParams } from "@/core/repositories/pagination-params";
-import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
-import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
+  public items: AnswerComment[] = []
+
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
+
   findManyByAswerId(
     answerId: string,
     params: PaginationParams,
   ): Promise<AnswerComment[] | null> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
 
-  public items: AnswerComment[] = [];
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        // vai la no repositorio de estudantes e pega as info. do estudante
+        // que é author desse comentário
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        // se não existe, erro..
+        if (!author) {
+          throw new Error(
+            `Author with ID ${comment.authorId.toString()} does not exist`,
+          )
+        }
+
+        // se existe, retorna um CommentWithAuthor
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        })
+      })
+
+    return answerComments
+  }
 
   async create(answerComment: AnswerComment) {
-    this.items.push(answerComment);
+    this.items.push(answerComment)
   }
 
   async findById(id: string) {
-    const answerComment = this.items.find((item) => item.id.toString() === id);
+    const answerComment = this.items.find((item) => item.id.toString() === id)
 
     if (!answerComment) {
-      return null;
+      return null
     }
 
-    return answerComment;
+    return answerComment
   }
 
   async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
     const answers = this.items
       .filter((item) => item.answerId.toString() === answerId)
-      .slice((page - 1) * 20, page * 20);
+      .slice((page - 1) * 20, page * 20)
 
-    return answers;
+    return answers
   }
 
   async delete(answerComment: AnswerComment) {
     const itemIndex = this.items.findIndex(
       (item) => item.id === answerComment.id,
-    );
+    )
 
-    this.items.splice(itemIndex, 1);
+    this.items.splice(itemIndex, 1)
   }
 }
