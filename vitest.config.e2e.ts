8750012ccfc32ccbcf6f239bbebadfda54723579
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2025-02-03 10:19:59 -0400

    fix: linting on `vitest.config.e2e.ts`

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,23 +1,23 @@
-import path from "node:path";
-import swc from "unplugin-swc";
-import { defineConfig } from "vitest/config";
+import path from 'node:path'
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     // só isso de diferença (pegar testes apenas com essa extensão)
-    include: ["**/*.e2e-spec.ts"],
+    include: ['**/*.e2e-spec.ts'],
     globals: true,
-    root: "./",
-    setupFiles: ["./test/setup-e2e.ts"], // arquivo que roda antes dos testes
+    root: './',
+    setupFiles: ['./test/setup-e2e.ts'], // arquivo que roda antes dos testes
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Mapeia @ para o diretório src
+      '@': path.resolve(__dirname, 'src'), // Mapeia @ para o diretório src
     },
   },
   plugins: [
     swc.vite({
-      module: { type: "es6" },
+      module: { type: 'es6' },
     }),
   ],
-});
+})
